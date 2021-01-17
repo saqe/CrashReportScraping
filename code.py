bdf943487bb058bdfdcc0d50dad928f7c445922e
Enigma <18554574+saqe@users.noreply.github.com>
2021-01-18 00:25:19 +0500

    Added requirements and modules

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
-print("Init")
+from util.email import SendInBlueMailer
+from os import getenv
+
+from dotenv import load_dotenv
+load_dotenv()
+
+mail=SendInBlueMailer(
+    API_KEY=getenv('SMTP_API_KEY'),
+    TO=getenv('EMAIL_TO'),
+)
+
