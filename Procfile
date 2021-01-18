ca7fd82687588f57efd5d70a448229c82a2052ec
Enigma <18554574+saqe@users.noreply.github.com>
2021-01-18 17:48:23 +0500

    Added flask

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python code.py 
+web: gunicorn code:app
--- code.py
+++ code.py
@@ -1,15 +1,33 @@
 from os import getenv
 import sentry_sdk
+from flask import Flask
 
 from scraper import Scraper
 
 from dotenv import load_dotenv
 load_dotenv()
 
+app = Flask(__name__)
+
 sentry_sdk.init(
     getenv('SENTRY_API_LINK'),
     traces_sample_rate=1.0
 )
 
-scraper=Scraper()
-scraper.start_scraping()
+@app.route('/start-the-scraper')
+def hello_name():
+    scraper=Scraper()
+    scraper.start_scraping()
+    return 'Started'
+
+@app.route('/')
+def wellcome():
+   return 'Hello world'
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
+
